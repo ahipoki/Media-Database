44cf0fa131a7ff0697034bcddac6092ac1f112cc
ahipoki <50777654+ahipoki@users.noreply.github.com>
2019-10-28 13:44:29 -0700

    Update VideoGame.cpp

--- VideoGame.cpp
+++ VideoGame.cpp
@@ -1,2 +1,24 @@
 #include <iostream>
 using namespace std;
+
+void videoGame : public Media
+{
+  char publisher[80];
+  float rating;
+}
+
+getTitle() {//Method for getting the title
+
+}
+
+getYear() {//Method for getting the year
+
+}
+
+getPublisher() {//Method for getting the publisher
+
+}
+
+getRating() {//Method for getting the rating
+
+}
