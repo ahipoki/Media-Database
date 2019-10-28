e009267f0787ae2aebb0dbc9137bdec36b52ab37
ahipoki <50777654+ahipoki@users.noreply.github.com>
2019-10-28 10:51:45 -0700

    Update MediaDatabase.cpp

--- MediaDatabase.cpp
+++ MediaDatabase.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+class Media
+{
+public:
+  char title[10];
+  int year;
+};
 
+class VideoGames : public Media
+{
+public:
+  char publisher[10];
+  int rating;
+}
+
+class Music : public Media
+{
+public:
+  char artist[10];
+  float duration;
+  char publisher[10];
+}
+
+class Movies : public Media
+{
+public:
+  char director[10];
+  float duration;
+  int rating;
+}
+
+int main() {
+  VideoGames game;
+  game.rating = 1;
+  cout << 
 }
