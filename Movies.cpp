351ccbd4c145b103cb3eb6f37a2941c5469527db
ahipoki <50777654+ahipoki@users.noreply.github.com>
2019-10-28 13:47:01 -0700

    Update Movies.cpp

--- Movies.cpp
+++ Movies.cpp
@@ -1,2 +1,28 @@
 #include <iostream>
 using namespace std;
+
+void movies() {
+  char director[80];
+  float duration;
+  float rating;
+}
+
+char getTitle() {
+  return char[80];
+}
+
+char getDirector() {
+  return char[80];
+}
+
+int getYear() {
+  return int;
+}
+
+float getDuration() {
+  return float;
+}
+
+float getRating() {
+  return float;
+}
