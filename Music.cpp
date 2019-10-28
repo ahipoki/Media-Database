621882a0b9114ee66fd48e63960e5657ccc57c19
ahipoki <50777654+ahipoki@users.noreply.github.com>
2019-10-28 13:50:34 -0700

    Update Music.cpp

--- Music.cpp
+++ Music.cpp
@@ -1,2 +1,28 @@
 #include <iostream>
 using namespace std;
+
+void music() {
+  char artist[80];
+  float duration;
+  char publisher[80];
+}
+
+char getTitle() {
+  return char[80];
+}
+
+char getArtist() {
+  return char[80];
+}
+
+int getYear() {
+  return int;
+}
+
+float getDuration() {
+  return float;
+}
+
+char getPublisher() {
+  return char[80];
+}
