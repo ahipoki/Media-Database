c79d45a1efcecc5b22df69e19146072d57b7a7ec
ahipoki <50777654+ahipoki@users.noreply.github.com>
2019-10-28 13:32:59 -0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,38 +1,28 @@
 #include <iostream>
 using namespace std;
 
-class Media
-{
-public:
-  char title[10];
-  int year;
-};
-
-class VideoGames : public Media
-{
-public:
-  char publisher[10];
-  int rating;
-}
-
-class Music : public Media
-{
-public:
-  char artist[10];
-  float duration;
-  char publisher[10];
-}
-
-class Movies : public Media
-{
-public:
-  char director[10];
-  float duration;
-  int rating;
-}
-
 int main() {
-  VideoGames game;
-  game.rating = 1;
-  cout << 
+  vector<Media*> *vecM = new vector<Media*>;
+  while (true) {
+    cout << "Do you want to add, search, delete or quit? (ADD/SEARCH/DELETE/QUIT)" << endl;
+    char input[80];
+    cin.getline(input, sizeof(input));
+    if (strcmp(input, "ADD") == 0) {
+      addMedia(vecM);
+      cin.ignore(1000, '\n');
+    }
+    else if (strcmp(input, "SEARCH") == 0) {
+      searchMedia(vecM);
+    }
+    else if (strcmp(input, "DELETE") == 0) {
+      deleteMedia(vecM);
+      cin.ignore(1000, '\n');
+    }
+    else if (strcmp(input, "QUIT") == 0) {
+      return false;
+    }
+    else {
+      cout << "That is an invalid option." << endl;
+    }
+  }
 }
