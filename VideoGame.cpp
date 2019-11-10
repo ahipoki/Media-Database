6f72ca4573d1762e7cf26c14ecfa8ed7db22bd30
ahipoki <50777654+ahipoki@users.noreply.github.com>
2019-11-10 09:53:59 -0800

    Update VideoGame.cpp

--- VideoGame.cpp
+++ VideoGame.cpp
@@ -1,21 +1,18 @@
 #include <iostream>
-#include <string.h>
+#include <cstring>
 #include "VideoGame.h"
 using namespace std;
 
-VideoGame::VideoGame(string* newtype)
+VideoGame::VideoGame(char[] newTitle, int newID, char[] newPublisher, int newRating, int newType)
 {
-  type = newtype;
-  id = 0;
-  //publisher
-  rating = 0.0;
+  title = newTitle;
+  id = newID;
+  publisher = newPublisher
+  rating = newRating;
+  type = newType;
 }
 
-string* VideoGame::getType() {
-  return type;
-}
-
-string* VideoGame::getTitle() {//Method for getting the title
+char* VideoGame::getTitle() {//Method for getting the title
   cout<<"Title"<<endl;
   //return char[80];
 }
@@ -25,17 +22,17 @@ int VideoGame::getYear() {//Method for getting the year
   //return int;
 }
 
-string* VideoGame::getPublisher() {//Method for getting the publisher
+char* VideoGame::getPublisher() {//Method for getting the publisher
   cout<<"Publisher"<<endl;
   //return char[80];
 }
 
-float VideoGame::getRating() {//Method for getting the rating
+int VideoGame::getRating() {//Method for getting the rating
   cout<<"Rating"<<endl;
   //return float;
 }
 
-//string* VideoGame::getType() {
-//cout<<"Type"<<endl;
-  //return int;
-//}
+int VideoGame::getType() {
+  cout<<"Type"<<endl;
+  return int;
+}
