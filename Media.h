b505bd33566ff12b29597f73fa41c58ca8c11968
ahipoki <50777654+ahipoki@users.noreply.github.com>
2019-11-19 19:46:54 -0800

    Update Media.h

--- Media.h
+++ Media.h
@@ -7,17 +7,24 @@
 using namespace std;
 
 class Media
-{
+{//Media.h file
   public:
     Media(char*, int);
     char* getTitle();
+    //Get title
     int getYear();
+    //Get year
     virtual int getType();
+    //Get type
     virtual ~Media();
+    //Media destructor
   private:
     char* title;
+    //Title
     int year;
+    //Year
     int type;
+    //Type
 };
 
 #endif
