0a6a6151f6ab077184bb076468fb02253505e36f
ahipoki <50777654+ahipoki@users.noreply.github.com>
2019-11-19 19:46:00 -0800

    Update Media.cpp

--- Media.cpp
+++ Media.cpp
@@ -3,28 +3,31 @@
 #include "Media.h"
 
 Media::Media(char* newTitle, int newYear) 
-{
+{//Media.cpp class
   title = new char[strlen(newTitle)+1];
+  //New title
   strcpy(title, newTitle);
+  //Copy to the new title
   year = newYear;
+  //New year
 }
 
 char* Media::getTitle() 
-{
+{//Get title method
   return title;
 }
 
 int Media::getYear() 
-{
+{//Get year method
   return year;
 }
 
 int Media::getType() 
-{
+{//Get type method
   return 0;
 }
 
 Media::~Media() 
-{
+{//Media destructor
   delete []title;
 }
