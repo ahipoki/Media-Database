8c00f7d29f36ffd46d8cc0145acd91d8aab41bbe
ahipoki <50777654+ahipoki@users.noreply.github.com>
2019-11-20 07:45:19 -0800

    Update Movie.cpp

--- Movie.cpp
+++ Movie.cpp
@@ -5,15 +5,19 @@
 using namespace std;
 
 Movie::Movie(char* newTitle, int newYear, char* newDirector, float newDuration, int newRating) : Media(newTitle, newYear) 
-{
+{//Movie.cpp file
   director = new char[strlen(newDirector)+1];
+  //New director
   strcpy(director, newDirector);
+  //Copy director to the new director
   duration = newDuration;
+  //New duration
   rating = newRating;
+  //New rating
 }
 
 char* Movie::getDirector() 
-{
+{//Get director method
   return director;
 }
 
