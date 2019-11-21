bd144b74eb7890d3eca56028a64958e89f3dad86
ahipoki <50777654+ahipoki@users.noreply.github.com>
2019-11-20 21:08:13 -0800

    Update Movie.h

--- Movie.h
+++ Movie.h
@@ -3,17 +3,25 @@
 #include "Media.h"
 
 class Movie : public Media
-{
+{//Movie.h file
  public:
   Movie(char*, int, char*, float, int);
   char* getDirector();
+  //Get director
   float getDuration();
+  //Get duration
   int getRating();
+  //Get rating
   int getType();
+  //Get type
   ~Movie();
+  //Movie destructor
  private:
   char* director;
+  //Director
   float duration;
+  //Duration
   int rating;
+  //Rating
 };
 
