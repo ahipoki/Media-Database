e3312a3ef3b8add37e1db6808f26ddb7c06551d7
ahipoki <50777654+ahipoki@users.noreply.github.com>
2019-11-21 07:33:26 -0800

    Update VideoGame.h

--- VideoGame.h
+++ VideoGame.h
@@ -2,16 +2,26 @@
 #include <cstring>
 #include "Media.h"
 
+/* Finn McKinley
+ * 11/19/19
+ * VideoGame.h file
+ */
 
-
-class VideoGame : public Media{
+class VideoGame : public Media
+{//.h file of video game class
  public:
   VideoGame(char*, int, char*, int);
   char* getPublisher();
+  //Get publisher
   int getRating();
+  //Get rating
   int getType();
+  //Get type of media
   ~VideoGame();
+  //Video game destructor
  private:
   char* publisher;
+  //Publisher
   int rating;
+  //Rating
 };
