fb87975a8f1acf48fad7c04bfb2ed6b2429d749b
ahipoki <50777654+ahipoki@users.noreply.github.com>
2019-11-21 07:30:44 -0800

    Update Music.h

--- Music.h
+++ Music.h
@@ -2,17 +2,30 @@
 #include <cstring>
 #include "Media.h"
 
+/* Finn McKinley
+ * 11/19/19
+ * Music.h file
+ */
+
 class Music : public Media
-{
+{//.h of music class
  public:
   Music(char*, int, char*, float, char*);
   char* getArtist();
+  //Get artist
   float getDuration();
+  //Get duration
   char* getPublisher();
+  //Get publisher
   int getType();
+  //Get type of media
   ~Music();
+  //Music destructor
  private:
   char* artist;
+  //Artist
   float duration;
+  //Duration
   char* publisher;
+  //Publisher
 };
