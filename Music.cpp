c42d993cee681e071522b6f1eb5986b05a4bd07a
ahipoki <50777654+ahipoki@users.noreply.github.com>
2019-11-20 21:10:31 -0800

    Update Music.cpp

--- Music.cpp
+++ Music.cpp
@@ -4,36 +4,46 @@
 #include "Media.h"
 
 Music::Music(char* newTitle, int newYear, char* newArtist, float newDuration, char* newPublisher) : Media(newTitle, newYear)
-{
+{//Music.cpp file
   artist = new char[strlen(newArtist)+1];
+  //New artist
   strcpy(artist, newArtist);
+  //Copy artist onto the new artist
   duration = newDuration;
+  //Duration equals new duration
   publisher = new char[strlen(newPublisher)+1];
+  //New publisher
   strcpy(publisher, newPublisher);
+  //Copy publisher onto the new publisher
 }
 
 char* Music::getArtist() 
-{
+{//Get artist method
   return artist;
+  //Return artist
 }
 
 float Music::getDuration() 
-{
+{//Get duration method
   return duration;
+  //Return duration
 }
 
 char* Music::getPublisher() 
-{
+{//Get publisher method
   return publisher;
+  //Return publisher
 }
 
 int Music::getType() 
-{
+{//Get media type method
   return 3;
+  //Return 3
 }
 
 Music::~Music() 
-{
+{//Music destructor
   delete artist;
   delete publisher;
+  //Delete char arrays
 }
